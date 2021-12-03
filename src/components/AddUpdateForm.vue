a25c4892ed156ad43fc794b106e8d05e6718a2f9
Sydney Goodwill <sydneyjogoodwill@gmail.com>
2021-12-03 10:57:51 -0700

    updated styling for add event modal

--- src/components/AddUpdateForm.vue
+++ src/components/AddUpdateForm.vue
@@ -31,3 +31,52 @@
     </form>
   </div>
 </template>
+
+<style scoped>
+.form_wrapper {
+  background: rgba(0, 0, 0, 0.7);
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+form {
+  background: white;
+  min-width: 60vw;
+  min-height: 40vh;
+  padding: 2rem;
+  border-radius: 0.3rem;
+}
+
+form > div {
+  display: flex;
+  flex-direction: column;
+  margin: 1rem 0;
+  font-size: 1.6rem;
+}
+
+form input, select {
+  margin: 0.6rem 0;
+  padding: 0.6rem 1rem;
+  border: 1px lightgray solid;
+  border-radius: 0.3rem;
+}
+
+form button {
+  background: rgb(123, 194, 123);
+  width: 100px;
+  border: none;
+  padding: 0.6rem;
+  border-radius: 0.3rem;
+  font-size: 16px;
+}
+
+label {
+  color: rgb(44, 43, 43);
+}
+</style>
